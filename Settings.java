3c2e71995e263955c77a4822aeb6cadfeed5148d
Gasteclair <gasteclair@gmail.com>
2018-02-25 19:59:57 +0100

    Add files via upload

---
+++ Settings.java
@@ -0,0 +1,14 @@
+package net.graphics;
+
+import java.awt.Color;
+
+public class Settings {
+
+	public static final int DISPLAY_WIDTH = 1280;
+	public static final int DISPLAY_HEIGHT = 720;
+	public static final String DISPLAY_NAME = "LWJGL VR Engine";
+	public static final float[] OVR_FOVS = new float[] {1.43f,1.43f,1.43f,1.43f};
+	public static final float BETWEEN_EYES_DISTANCE = 0.4f;
+	public static final Color CLEAR_COLOR = Color.BLUE;
+	
+}
